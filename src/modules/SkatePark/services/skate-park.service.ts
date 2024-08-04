052509ba365598040562971dd01a58c999908649
Brazlucas <107437261+Brazlucas@users.noreply.github.com>
2024-08-04 20:26:15 -0300

    ✨ created: new entities and interfaces to skate-park and services and base http service

---
+++ src/modules/SkatePark/services/skate-park.service.ts
@@ -0,0 +1,31 @@
+import { AxiosResponse } from 'axios';
+import http from '@/services/base/http.service';
+import SkatePark from '../entities/skate-park.entity';
+
+class SkateParkService {
+  public async create(data: SkatePark) {
+    return http
+      .post('/skate-parks', data);
+  }
+  public async update(data: SkatePark) {
+    return http
+      .put(`/skate-parks/${data.id}`, data);
+  }
+  public async getAll() {
+    return http
+      .get('/skate-parks')
+      .then(({ data }: AxiosResponse) => data);
+  }
+  public async getById(id: string) {
+    return http
+      .get(`/skate-parks/${id}`)
+      .then(({ data }: AxiosResponse) => data);
+  }
+  public async delete(id: string) {
+    return http
+      .delete(`/skate-parks/${id}`)
+      .then(({ data }: AxiosResponse) => data);
+  }
+}
+export default new SkateParkService();
+
--- src/services/base/http.service.ts
+++ src/services/base/http.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8080';
+const baseURL = 'http://127.0.0.1:8000/api';
 
 const http = axios.create({
   baseURL
