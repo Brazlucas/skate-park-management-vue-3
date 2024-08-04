b80705d51fb06ce9089a635fd4b726494494ff82
Brazlucas <107437261+Brazlucas@users.noreply.github.com>
2024-08-04 15:18:08 -0300

    🎉 beginning front

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <section class="app-grid">
+      <div class="app-content">
+        <v-main>
+          <v-container class="pa-0" fluid>
+            <router-view></router-view>
+          </v-container>
+        </v-main>
+      </div>
+    </section>
+  </v-app>
+</template>
+
+<style lang="scss">
+.app-grid {
+  display: flex;
+}
+
+.app-menu {
+  width: auto;
+}
+.app-content {
+  padding: 30px 50px 50px;
+  width: 100%;
+}
+</style>
